4dd8178d4029c3e304b285666c6b9b3d6bf5e832
Kajtekaktus <stachowskikajetan@gmail.com>
2024-03-08 01:20:58 +0100

    changed background

---
+++ app.js
@@ -0,0 +1,19 @@
+
+const arrows = document.getElementById("arrows-img");
+const body = document.querySelector("body");
+
+window.addEventListener("scroll", () => {
+  console.log(window.scrollY);
+  if (window.scrollY > 100) {
+    // setTimeout(() => {
+    arrows.classList.add("hidden");
+    // }, 1000);
+  } else {
+    arrows.classList.remove("hidden");
+  }
+});
+
+arrows.addEventListener("click", function () {
+  console.log("dzakdas");
+  arrows.scrollIntoView({ behavior: "smooth" });
+});
